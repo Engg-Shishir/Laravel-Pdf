31f8148f34b116a2f683d491cfdba153128686b4
Shishir Bhuiyan <shishirbhuiyan83@gmail.com>
2022-10-04 11:21:03 +0600

    print view as a pdf

--- app/app/Http/Controllers/pdfController.php
+++ app/app/Http/Controllers/pdfController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class pdfController extends Controller
 {
     
     function index(){
-        $pdf = App::make('dompdf.wrapper');
-        $pdf->loadHTML('<h1>Test</h1>');
-        return $pdf->stream();
+        // $pdf = App::make('dompdf.wrapper');
+        // $pdf->loadHTML('<h1>Test</h1>');
+        // return $pdf->stream();
+
+        $data = "Shishir Bhuiyan";
+
+        $pdf = Pdf::loadView('hellopdf');
+        return $pdf->download('invoice.pdf');
     }
 }
